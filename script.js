bea66424d332daf7c59c71ef4a4b965f89b944e6
FathimaDileep <fathimadileep2019@gmail.com>
2024-03-25 20:59:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+console.log('Hello');
+//Variables
+var a=9;
+console.log(a);
+var a=23;
+console.log(a);
+{
+var a=22;
+
+}
+console.log(a);
+a=90;
+console.log(a);
+//let declaration
+let b=80;
+console.log(b);
+{
+    let b=70;
+    console.log(b);
+}
+console.log(b);
+b=90;
+console.log(b);
+//const declaration
+const ti=890;//Number
+{
+    const ti=8;
+    console.log(ti);
+    
+}
+console.log(typeof(b));
+var nam='Miya';
+console.log(typeof (nam));
+var isNumber=true;//Boolean
+var g;
+console.log(typeof(g));
+//null
+let lastName=null;
+console.log(typeof(lastName));
+//Arrays
+var arr1=[12,35,56,'Heera'];
+console.log(arr1[3]);
+arr1.push(24);
+console.log(arr1);
+
+
+    
